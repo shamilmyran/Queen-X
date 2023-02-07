8015f9db4f405e30a90c3f5d66641349f962f533
shamilmyran <97163572+shamilmyran@users.noreply.github.com>
2023-02-07 10:45:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ const chalk = require('chalk')
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
